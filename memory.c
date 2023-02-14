325099c2117528446611ebba3d3fe4cd28c0d438
Jhunyingan <jhunyingan1@gmail.com>
2023-02-13 18:59:46 -0800

    13files

---
+++ memory.c
@@ -0,0 +1,25 @@
+/*
+ * Auth: Philip Boluwatife
+ * Col: John Hunyingan
+ * File: memory.c
+ */
+
+#include "shell.h"
+
+/**
+ * bfree - frees a pointer and NULLs the address
+ * @ptr: address of the pointer to free
+ *
+ * Return: 1 if freed, otherwise 0.
+ */
+
+int bfree(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
